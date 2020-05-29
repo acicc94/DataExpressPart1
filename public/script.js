63c8cc08a3bc2bca5de3d5d77b81c62f4dfd95c3
DRoberts919 <60489717+DRoberts919@users.noreply.github.com>
2020-05-29 13:32:55 -0600

    graphs
    grpah work

--- public/script.js
+++ public/script.js
@@ -5,14 +5,35 @@ canvas.width = 600;
 canvas.height = 400;
 
 const drawStuff = () =>{
-    ctx.fillRect(0,300,30,100)
+    ctx.fillRect(0,300,25,100)
     ctx.fillStyle = '#ff1493';
 
-    ctx.fillRect(25,300, 30,100)
+    ctx.fillRect(35,300, 25,100);
     ctx.fillStyle = '#ff0000';
+    ctx.fillText("secondBox",45,300);
+    ctx.strokeText("secondBox", 45, 300);
 
-
-
+    ctx.fillRect(65,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    
+    ctx.fillRect(95,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(115,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(125,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(135,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(165,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(195,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(225,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(255,300, 25,100);
+    ctx.fillStyle = '#ff1493';
+    ctx.fillRect(285,300, 25,100);
+    ctx.fillStyle = '#ff1493';
 };
 
 
