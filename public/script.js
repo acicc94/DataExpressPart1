f4a8e6898c361505e826e4c4a2954dd4c7075555
DRoberts919 <60489717+DRoberts919@users.noreply.github.com>
2020-06-03 13:34:30 -0600

    bar graph
    lkl

--- public/script.js
+++ public/script.js
@@ -35,70 +35,70 @@ const api_data = (my_data) => {
 
     //blue bar
     ctx.fillStyle = "#220BD6";
-    ctx.fillRect(35, blueHeight, 25, 500);
-    ctx.fillText("Blue", 35, 400);
-    ctx.strokeText("Blue", 35, 400);
+    ctx.fillRect(55, blueHeight, 25, 500);
+    ctx.fillText("Blue", 55, 400);
+    ctx.strokeText("Blue", 55, 400);
 
     //green bar
     ctx.fillStyle = "#00D624";
-    ctx.fillRect(65, greenHeight, 25, 500);
-    ctx.fillText("Green", 65, 400);
-    ctx.strokeText("Green", 65, 400);
+    ctx.fillRect(105, greenHeight, 25, 500);
+    ctx.fillText("Green", 105, 400);
+    ctx.strokeText("Green", 105, 400);
 
     //yellow bar
     ctx.fillStyle = "#FFE400";
-    ctx.fillRect(95, yellowHeight, 25, 500);
-    ctx.fillText("Yellow", 95, 400);
-    ctx.strokeText("Yellow", 95, 400);
+    ctx.fillRect(155, yellowHeight, 25, 500);
+    ctx.fillText("Yellow", 155, 400);
+    ctx.strokeText("Yellow", 155, 400);
   };
 
   foodBars = () => {
     //pizza
     ctx.fillStyle = "#D4AC42";
-    ctx.fillRect(125, pizzaHeight, 25, 500);
-    ctx.fillText("Pizza", 125, 400);
-    ctx.strokeText("Pizza", 125, 400);
+    ctx.fillRect(205, pizzaHeight, 25, 500);
+    ctx.fillText("Pizza", 205, 400);
+    ctx.strokeText("Pizza", 205, 400);
     //cheese burger
     ctx.fillStyle = "#AD0BD6";
-    ctx.fillRect(155, cheesBurgerHeight, 25, 500);
-    ctx.fillText("Burger", 155, 400);
-    ctx.strokeText("Burger", 155, 400);
+    ctx.fillRect(255, cheesBurgerHeight, 25, 500);
+    ctx.fillText("Burger", 255, 400);
+    ctx.strokeText("Burger", 255, 400);
     //icecream
     ctx.fillStyle = "#5DFFB7";
-    ctx.fillRect(185, iceCreamHeight, 25, 500);
-    ctx.fillText("IceCream", 185, 400);
-    ctx.strokeText("IceCream", 185, 400);
+    ctx.fillRect(305, iceCreamHeight, 25, 500);
+    ctx.fillText("IceCream", 305, 400);
+    ctx.strokeText("IceCream", 305, 400);
     //steak
     ctx.fillStyle = "#F07013";
-    ctx.fillRect(215, SteakHeight, 25, 500);
-    ctx.fillText("Steak", 215, 400);
-    ctx.strokeText("Steak", 215, 400);
+    ctx.fillRect(355, SteakHeight, 25, 500);
+    ctx.fillText("Steak", 355, 400);
+    ctx.strokeText("Steak", 355, 400);
   };
 
   musicBars = () => {
     //classical
     ctx.fillStyle = "#ff1493";
-    ctx.fillRect(245, classicHeight, 25, 500);
-    ctx.fillText("Classical", 245, 400);
-    ctx.strokeText("Classical", 245, 400);
+    ctx.fillRect(405, classicHeight, 25, 500);
+    ctx.fillText("Classical", 405, 400);
+    ctx.strokeText("Classical", 405, 400);
 
     //rock
     ctx.fillStyle = "#7099FF";
-    ctx.fillRect(275, rockHeight, 25, 500);
-    ctx.fillText("Rock", 275, 400);
-    ctx.strokeText("Rock", 275, 400);
+    ctx.fillRect(455, rockHeight, 25, 500);
+    ctx.fillText("Rock", 455, 400);
+    ctx.strokeText("Rock", 455, 400);
 
     //rap
     ctx.fillStyle = "#D9D35D";
-    ctx.fillRect(305, rapHeight, 25, 500);
-    ctx.fillText("Rap", 305, 400);
-    ctx.strokeText("Rap", 305, 400);
+    ctx.fillRect(505, rapHeight, 25, 500);
+    ctx.fillText("Rap", 505, 400);
+    ctx.strokeText("Rap", 505, 400);
 
     //edm
     ctx.fillStyle = "#3776FF";
-    ctx.fillRect(335, edmHeight, 25, 500);
-    ctx.fillText("EDM", 335, 400);
-    ctx.strokeText("EDM", 335, 400);
+    ctx.fillRect(555, edmHeight, 25, 500);
+    ctx.fillText("EDM", 555, 400);
+    ctx.strokeText("EDM", 555, 400);
   };
 };
 
